86150d96791b4d767a446af6f72f786df7e432c5
Chien <zqy0224@live.com>
2021-10-18 17:42:44 +0800

    stage

--- src/Arcbot/Program.cs
+++ src/Arcbot/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
+using System.Threading.Tasks;
+using Ac682.Extensions.Logging.Console;
+using Arcbot.Services;
+using HyperaiX;
+using HyperaiX.Clients;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Arcbot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Host
+                .CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(configure => configure
+                    .AddTomlFile("arc.is.bot.toml"))
+                .ConfigureServices((context, services) => services
+                    .AddLogging(builder => builder
+                        .ClearProviders()
+                        .AddConsole(configure => configure
+                            .AddBuiltinFormatters()))
+                    .AddOnebot(context.Configuration.GetSection("Onebot"))
+                    .AddHyperaiX(configure => configure
+                        .UseEventBlocker()
+                        .UseUnits()))
+                .UseConsoleLifetime()
+                .Build()
+                .Run();
         }
     }
 }
