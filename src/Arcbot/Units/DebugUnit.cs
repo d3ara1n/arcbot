dc305b6f9ac01891cf5695f93ab66aab71fbf889
Chien Zhang <zqy0224@live.com>
2022-04-14 19:27:01 +0800

    Not work

--- src/Arcbot/Units/DebugUnit.cs
+++ src/Arcbot/Units/DebugUnit.cs
@@ -9,6 +9,7 @@ using HyperaiX.Abstractions.Messages.ConcreteModels;
 using HyperaiX.Abstractions.Relations;
 using HyperaiX.Units;
 using HyperaiX.Units.Attributes;
+using Microsoft.Extensions.Logging;
 using Quartz;
 using Image = HyperaiX.Abstractions.Messages.ConcreteModels.Image;
 
@@ -17,21 +18,23 @@ namespace Arcbot.Units
     public class DebugUnit : UnitBase
     {
         private readonly ISchedulerFactory _schedulerFactory;
+        private readonly ILogger _logger;
 
-        public DebugUnit(ISchedulerFactory schedulerFactory)
+        public DebugUnit(ISchedulerFactory schedulerFactory, ILogger<DebugUnit> logger)
         {
             _schedulerFactory = schedulerFactory;
+            _logger = logger;
         }
 
         [Receiver(MessageEventType.Group | MessageEventType.Friend)]
-        [Handler("!ping")]
+        [Extract("!ping")]
         public string Ping()
         {
             return "pong!";
         }
         
         [Receiver(MessageEventType.Group | MessageEventType.Friend)]
-        [Handler("!version")]
+        [Extract("!version")]
         public MessageChain Version()
         {
             return MessageChain.Construct(new Plain("IDK"));
@@ -45,10 +48,18 @@ namespace Arcbot.Units
         }
 
         [Receiver(MessageEventType.Group | MessageEventType.Friend)]
-        [Handler("!echo {chain}")]
+        [Extract("!echo {chain}")]
         public MessageChain Echo(MessageChain chain)
         {
             return chain;
         }
+
+        [Receiver(MessageEventType.Group)]
+        [Extract("OMG{face:Face}")]
+        public void Punch(MessageChain chain, Face face)
+        {
+            _logger.LogInformation(chain.Flatten());
+        }
+
     }
 }
