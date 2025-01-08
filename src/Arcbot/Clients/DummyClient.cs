b58ee11b7dcb89b9086fd8cc873904a511024719
Chien Zhang <d3ara1n@outlook.com>
2025-01-08 16:37:04 +0800

    add dummy client

---
+++ src/Arcbot/Clients/DummyClient.cs
@@ -0,0 +1,54 @@
+﻿using HyperaiX.Abstractions;
+using HyperaiX.Abstractions.Actions;
+using HyperaiX.Abstractions.Events;
+using HyperaiX.Abstractions.Messages;
+using HyperaiX.Abstractions.Messages.Payloads;
+using HyperaiX.Abstractions.Messages.Payloads.Elements;
+using HyperaiX.Abstractions.Receipts;
+using HyperaiX.Extensions.QQ.Roles;
+
+namespace Arcbot.Clients;
+
+public class DummyClient : IEndClient
+{
+    private static readonly Friend MISIDE = new Friend(114514, "米塔");
+
+    private GenericEventArgs[] _events =
+    [
+        new MessageEventArgs(new Conversation(MISIDE), MISIDE,
+            new MessageEntity(new RichContent([new Text("这是第一条消息")]), new Dictionary<string, object>(),
+                DateTimeOffset.UtcNow))
+    ];
+
+    private int _cursor = 0;
+
+    public Task ConnectAsync(CancellationToken token)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DisconnectAsync(CancellationToken token)
+    {
+        return Task.CompletedTask;
+    }
+
+    public GenericEventArgs Read(CancellationToken token)
+    {
+        if (_cursor < _events.Length)
+        {
+            return _events[_cursor++];
+        }
+
+        while (true)
+        {
+            token.ThrowIfCancellationRequested();
+            Thread.Sleep(1000);
+        }
+        
+    }
+
+    public GenericReceiptArgs Write(GenericActionArgs action, CancellationToken token)
+    {
+        return new GenericReceiptArgs();
+    }
+}
--- src/Arcbot/Program.cs
+++ src/Arcbot/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Arcbot.Clients;
+using HyperaiX;
+using HyperaiX.Abstractions;
+using HyperaiX.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var app = Host.CreateApplicationBuilder(args);
+
+app.Services.AddHyperaiX(options =>
+{
+    options
+        .UseLogging()
+        .UseBlacklist()
+        .UseBots();
+});
+app.Services.AddSingleton<IEndClient, DummyClient>();
+
+app.Build().Run();
